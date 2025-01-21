15adf57beca8e2d1ff68f3901b343e7f3f965cb2
luail <kynia0506@naver.com>
2025-01-21 17:22:20 +0900

    first

---
+++ src/main/java/com/TTT/TTT/TttApplication.java
@@ -0,0 +1,13 @@
+package com.TTT.TTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TTT/TTT/TttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TTT.TTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
