87743ff14b9c4c3080abb15fb7052b8c325da580
soby <vthquddbsv@gmail.com>
2025-02-05 17:39:55 +0900

    post 기초

---
+++ src/main/java/com/TTT/TTT/Post/controller/PostController.java
@@ -0,0 +1,43 @@
+package com.TTT.TTT.Post.controller;
+
+import com.TTT.TTT.Post.dtos.PostDetailRes;
+import com.TTT.TTT.Post.dtos.PostListRes;
+import com.TTT.TTT.Post.dtos.PostUpdateReq;
+import com.TTT.TTT.Post.service.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // 전체조회
+    @GetMapping
+    public List<PostListRes> getAllPost() {
+        return postService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PostDetailRes getPostById(@PathVariable Long id) {
+        return postService.findById(id); // ID로 게시글 조회 (수정가능)
+    }
+
+    // 게시글 수정
+    @PutMapping("/{id}")
+    public void updatePost(@PathVariable Long id, @RequestBody PostUpdateReq dto) {
+        postService.update(id, dto);
+    }
+
+    // 게시글 삭제
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        postService.delete(id);
+    }
+
+}
---
+++ src/main/java/com/TTT/TTT/Post/domain/Post.java
@@ -0,0 +1,48 @@
+package com.TTT.TTT.Post.domain;
+
+import com.TTT.TTT.User.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 500) // 대략 500자 설정
+    private String contents;
+
+    private LocalDateTime createdTime;
+
+    public void update(String title,String contents){
+        this.title=title;
+        this.contents=contents;
+    }
+
+    public void getContent() {
+    }
+
+
+//    private int likes; //좋아요는 아직 생략
+
+
+
+}
