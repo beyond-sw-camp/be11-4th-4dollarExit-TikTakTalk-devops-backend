a810d92d70c6fa4827d9884462b6b8b8079e89c6
jykim1187 <rifkehtm862@gmail.com>
2025-02-11 19:59:05 +0900

    Feat : 내가 좋아요 한 목록 조회 기능 추가

--- src/main/java/com/TTT/TTT/Likes/Service/LikesService.java
+++ src/main/java/com/TTT/TTT/Likes/Service/LikesService.java
@@ -27,13 +27,15 @@ public class LikesService {
 
 
 @Qualifier("likes")
-    private final RedisTemplate<String,Object> redisTemplate;
+    private final RedisTemplate<String,String> redisTemplate;
 
 
-    public LikesService(PostRepository postRepository, UserRepository userRepository, LikesRabbitmqService likesRabbitmqService, @Qualifier("likes")RedisTemplate redisTemplate) {
+    public LikesService(PostRepository postRepository, UserRepository userRepository
+            , LikesRabbitmqService likesRabbitmqService, @Qualifier("likes") RedisTemplate<String, String> redisTemplate) {
         this.postRepository = postRepository;
         this.userRepository = userRepository;
         this.likesRabbitmqService = likesRabbitmqService;
+
         this.redisTemplate = redisTemplate;
     }
 
@@ -47,15 +49,18 @@ public class LikesService {
 
         String likeUserKey = "post-" + postId +"-likeUsers"; //레디스에 해당 포스트에 좋아요 누른 목록사람에 대해 저장될 키값(밸류는 셋 자료구조를 이용한 좋아요 누른 사람 목록) ex.post-1-likeusers : 5 이렇게 저장될 수 있도록
         String likeCountKey = "post-" + postId + "-likeCount"; //레디스에 해당 포스트의 좋아요 개수와 관련한 키값(밸류는 좋아요 개수)
+        String likeMyListKey = "user-"+ loginId + "-myLikeList"; //레디스에 해당 유저가 좋아요한 포스트id를 저장.그에 대한 키값
 
         if(redisTemplate.opsForSet().isMember(likeUserKey,loginUser.getLoginId())){ //해당 포스트 좋아요 누른 유저 목록에 지금 유저 아이디가 있다면, 즉 이미 좋아요를 누른사람이라면
             redisTemplate.opsForSet().remove(likeUserKey,loginUser.getLoginId()); //해당 목록에서 현재 유저를 제거
             redisTemplate.opsForValue().decrement(likeCountKey);//그리고 좋아요 개수에서 1을 뺀다
+            redisTemplate.opsForSet().remove(likeMyListKey,String.valueOf(post.getId()));//내가 좋아료 한 글 목록에 해당 포스트 아이디를 삭제한다.
 
             likesRabbitmqService.publishForMinus(dto);//rbd에서 좋아요 삭제
         } else{
             redisTemplate.opsForSet().add(likeUserKey,loginUser.getLoginId());//해당 포스트 좋아요 누른 유저목록에 현재 유저를 추가
             redisTemplate.opsForValue().increment(likeCountKey);//그리고 좋아요 개수를 1증가시킨다
+            redisTemplate.opsForSet().add(likeMyListKey,String.valueOf(post.getId()));//내가 좋아요 한 글 목록에 해당 포스트 아이디를 저장시킨다.
 
             likesRabbitmqService.publishForAdding(dto); //rdb에서 좋아요 추가
             if(loginUser!=AuthorOfPost) {
