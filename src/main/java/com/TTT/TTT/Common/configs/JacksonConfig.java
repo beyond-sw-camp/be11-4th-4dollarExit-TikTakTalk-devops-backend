1eb0632c6b9d3474fabdee36ba5883c275c86baa
luail <kynia0506@naver.com>
2025-02-20 20:58:56 +0900

    Refactor : "실시간 챗에서도 시간표기될수 있게 수정." Json이 LocalDateTime도 파싱할 수 있도록 의존성 추가 및 코드 리팩터링 진행.

---
+++ src/main/java/com/TTT/TTT/Common/configs/JacksonConfig.java
@@ -0,0 +1,17 @@
+package com.TTT.TTT.Common.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        System.out.println("자바모듈 등록했당!");
+        return objectMapper;
+    }
+}
--- src/main/java/com/TTT/TTT/chat/dto/ChatMessageDto.java
+++ src/main/java/com/TTT/TTT/chat/dto/ChatMessageDto.java
@@ -1,5 +1,6 @@
 package com.TTT.TTT.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,5 +16,6 @@ public class ChatMessageDto {
     private Long roomId;
     private String message;
     private String senderNickName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime sendTime;
 }
