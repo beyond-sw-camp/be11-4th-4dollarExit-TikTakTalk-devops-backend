3dc0d2387612a01bd6148e4a32ad5cc2cc1b148a
luail <kynia0506@naver.com>
2025-02-24 19:26:54 +0900

    Refactor : " 전체 게시판을 제외한 나머지 게시판 본문 안나오는 부분 수정."

--- src/main/java/com/TTT/TTT/Post/dtos/PostListDto.java
+++ src/main/java/com/TTT/TTT/Post/dtos/PostListDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class PostListDto {
     private Long postId;
     private String title;
+    private String contents;
     private LocalDateTime createdTime;
     private String AuthorNickName;
     private String AuthorImage;
@@ -21,4 +22,5 @@ public class PostListDto {
     private int countOfComment;
     private int likesCount;
     private int viewCount;
+    private int AuthorRankingPoint;
 }
